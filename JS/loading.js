2d830a32b177da39305bba05757e47c40c2655bd
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-04-26 21:34:30 +0530

    2025.0.0.1.6

--- JS/loading.js
+++ JS/loading.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
   let loading = document.querySelector("#loading");
-  let home = document.querySelector(".home");
+  let home = document.querySelector(".home")[0];
 
   setTimeout(() => {
     loading.style.display = "none";
