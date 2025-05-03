2f41ba9134d281b2cab809764cbe6f20901eef69
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-05-04 00:13:45 +0530

    2025.0.0.1.6

--- JS/SignUpLogIn.js
+++ JS/SignUpLogIn.js
@@ -3,8 +3,10 @@ let sUBtn = document.querySelector("#signUpButton");
 sUBtn.addEventListener("click", () => {
   let sU = document.querySelector("#signUp");
   let lI = document.querySelector("#logIn");
+  let sUF = document.querySelector("#signUpF");
   sU.style.display = "flex";
   lI.style.display = "none";
+  sUF.reset();
 });
 
 let lIBtn = document.querySelector("#logInButton");
@@ -12,8 +14,10 @@ let lIBtn = document.querySelector("#logInButton");
 lIBtn.addEventListener("click", () => {
   let sU = document.querySelector("#signUp");
   let lI = document.querySelector("#logIn");
+  let lIF = document.querySelector("#logInF");
   sU.style.display = "none";
   lI.style.display = "flex";
+  lIF.reset();
 });
 
 
